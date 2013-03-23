e34807391e04fcb19a7594f313d196550c2ef253
2kan <dev@2kanman.net>
2013-03-23 18:54:45 +1100

    added new function class

---
+++ include/Function.h
@@ -0,0 +1,16 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <string>
+
+class Function
+{
+public:
+	std::string name;
+	int	lineStart;
+	int	lineEnd;
+};
+
+
+
+#endif // __FUNCTION_H__
---
+++ source/Function.cpp
@@ -0,0 +1,18 @@
+#include "Function.h"
+
+/*Function::Function()
+{
+	lineStart	= 0;
+	lineEnd		= 0;
+}
+
+Function::Function(int a_lineStart, int a_lineEnd)
+{
+	lineStart	= a_lineStart;
+	lineEnd		= a_lineEnd;
+}
+
+Function::~Function()
+{
+
+}*/
