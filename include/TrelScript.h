8a5eec0132d2a2c5cb8bc24dd48ced8b59e8d68f
2kan <dev@2kanman.net>
2013-04-02 23:39:52 +1100

    added hooks for new function triggers

--- include/TrelScript.h
+++ include/TrelScript.h
@@ -1,6 +1,9 @@
 #ifndef __TRELSCRIPT_H__
 #define __TRELSCRIPT_H__
 
+// Forward declaring
+class Script;
+
 class TrelScript
 {
 public:
@@ -9,9 +12,11 @@ public:
 	~TrelScript();
 
 	void runScript();
+	void addFunc(std::string a_funcName, void (*a_func)());
 private:
 	void setInfo();
 
+	Script *m_script;
 	std::string m_filepath;
 	std::string m_name;
 	std::string m_version;
--- source/TrelScript.cpp
+++ source/TrelScript.cpp
@@ -6,30 +6,36 @@
 TrelScript::TrelScript()
 {
 	m_filepath	= "";
+	m_script	= new Script(m_filepath);
 	setInfo();
 }
 
 TrelScript::TrelScript(std::string a_filepath)
 {
 	m_filepath	= a_filepath;
+	m_script	= new Script(m_filepath);
 	setInfo();
 }
 
 TrelScript::~TrelScript()
 {
+	//delete m_script;
 	std::cout << "Execution complete.\n";
 }
 
 void TrelScript::setInfo()
 {
 	m_name		= "Trel Script";
-	m_version	= "0.3.4";
+	m_version	= "0.4";
 }
 
 void TrelScript::runScript()
 {
 	std::cout << "~~ " + m_name + " v" + m_version + " ~~" << std::endl;
+	m_script->executeScript();
+}
 
-	Script *thisScript	= new Script(m_filepath);
-	thisScript->executeScript();
+void TrelScript::addFunc(std::string a_funcName, void (*a_func)())
+{
+	m_script->addLibFunction(a_funcName, a_func);
 }
